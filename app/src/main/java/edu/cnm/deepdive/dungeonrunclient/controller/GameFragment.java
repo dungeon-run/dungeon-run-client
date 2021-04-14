6afa2c4eeaba97b362e4ee737ba50891aecb7dfa
Hana Dawson <stubbornunicorn95@gmail.com>
2021-04-14 11:57:13 -0600

    Working on displaying game screen.

---
+++ app/src/main/java/edu/cnm/deepdive/dungeonrunclient/controller/GameFragment.java
@@ -0,0 +1,31 @@
+package edu.cnm.deepdive.dungeonrunclient.controller;
+
+import android.os.Bundle;
+import androidx.fragment.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import edu.cnm.deepdive.dungeonrunclient.R;
+
+public class GameFragment extends Fragment {
+
+  public static GameFragment newInstance(String param1, String param2) {
+    GameFragment fragment = new GameFragment();
+    Bundle args = new Bundle();
+    fragment.setArguments(args);
+    return fragment;
+  }
+
+  @Override
+  public void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+
+  }
+
+  @Override
+  public View onCreateView(LayoutInflater inflater, ViewGroup container,
+      Bundle savedInstanceState) {
+    // Inflate the layout for this fragment
+    return inflater.inflate(R.layout.fragment_game, container, false);
+  }
+}
--- app/src/main/java/edu/cnm/deepdive/dungeonrunclient/controller/PlayFragment.java
+++ app/src/main/java/edu/cnm/deepdive/dungeonrunclient/controller/PlayFragment.java
@@ -1,10 +1,12 @@
 package edu.cnm.deepdive.dungeonrunclient.controller;
 
 import android.os.Bundle;
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import androidx.navigation.Navigation;
 import edu.cnm.deepdive.dungeonrunclient.R;
 import edu.cnm.deepdive.dungeonrunclient.databinding.FragmentPlayBinding;
 
@@ -12,19 +14,18 @@ public class PlayFragment extends Fragment {
 
     FragmentPlayBinding binding;
 
-    public PlayFragment() {
-
-    }
-
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
         Bundle savedInstanceState) {
         binding = FragmentPlayBinding.inflate(inflater, container, false);
+//        binding.startGameButton.setOnClickListener((v) -> {
+//            Navigation.findNavController(binding.getRoot()).navigate(GameFragment);
+//        });
         return binding.getRoot();
     }
 }
